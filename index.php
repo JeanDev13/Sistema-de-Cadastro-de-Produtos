dabcf674b41433762c0d9daac8b477d1aa242fc0
JeanDev13 <jeanxavier2017@gmail.com>
2026-07-05 15:29:38 -0300

    Creating the product table in index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
     require "conect.php";
-
+    
+    $stmt = $pdo->query("SELECT * FROM produtos");
+    $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
 ?>
 
 <!doctype html>
@@ -9,10 +12,38 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>LOJA</title>
   </head>
   <body>
-        <>
-    <h1>Cadasto</h1>
+      <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">*</th>
+        <th scope="col">Produto</th>
+        <th scope="col">Preço</th>
+        <th scope="col">Quantidade</th>
+        <th scope="col">Categoria</th>
+      </tr>
+    </thead>
+    <tbody>
+        
+        <?php if (count($produtos) == 0): ?>
+        <tr>
+          <td colspan="5">Nenhum produto cadastrado ainda.</td>
+        </tr>
+        <?php else: ?>
+            <?php foreach($produtos as $produto):?>
+              <tr>
+                <th scope="row"><?= $produto["id"] ?></th>
+                <td><?= $produto["nome"] ?></td>
+                <td><?= $produto["preco"] ?></td>
+                <td><?= $produto["quantidade"] ?></td>
+                <td><?= $produto["categoria"] ?></td>
+              </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>    
+      </tbody>
+      </table>
   </body>
 </html>
