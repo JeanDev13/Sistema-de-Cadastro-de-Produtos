a14573188d6e268ac9343c736acab4141a95cc9f
JeanDev13 <jeanxavier2017@gmail.com>
2026-07-05 19:51:17 -0300

    Creating a system to delete products in delete.php

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php 
+
+    require "conect.php";
+
+    if(!isset($_GET["id"])){
+        header ("Location: index.php");
+        exit; 
+    }
+
+    $id = $_GET["id"];
+
+    $sql = ("DELETE FROM produtos WHERE id = :id");
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([":id" => $id]);
+
+    header ("Location: index.php");
+    exit;
+
+?>
